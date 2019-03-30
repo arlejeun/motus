f6fe6a9a6036371156a1dbc05d8bfb2a7c45cc3a
Arnaud Lejeune <lejeune.arnaud@gmail.com>
2019-03-29 22:24:22 -0500

    production copy

--- motus/settings/base.py
+++ motus/settings/base.py
@@ -95,11 +95,23 @@ WSGI_APPLICATION = 'motus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+##    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'localmotus',
+        'USER': 'motus',
+        'PASSWORD': 'ninja',
+        'HOST':'localhost',
+        'PORT':'5432',
+        'CONN_MAX_AGE': 600
+   }
 }
 
 
